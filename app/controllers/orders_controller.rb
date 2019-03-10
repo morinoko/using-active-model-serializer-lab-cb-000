cb5c9de93f357935adabe66d310e24e622cfa666
Felice Forby <feliceforby@gmail.com>
2019-03-10 23:14:14 +0000

    Done.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,14 @@
 class OrdersController < ApplicationController
-
   def index
     @orders = Order.all
   end
 
+  def show
+    @order = Order.find(params[:id])
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: order }
+    end
+  end
 end
