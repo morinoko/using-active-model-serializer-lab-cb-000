78ee43d08a3e0a6db3a6f34f2a5bc425b96220a8
Felice Forby <feliceforby@gmail.com>
2019-03-10 23:10:07 +0000

    Automatically backed up by Learn

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,13 @@
 class OrdersController < ApplicationController
-
   def index
     @orders = Order.all
   end
 
+  def show
+    order = Order.find(params[:id])
+
+    respond_to do |format|
+      format.json { render json: product }
+    end
+  end
 end
